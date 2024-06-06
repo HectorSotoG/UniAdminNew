3be44d3f1a09b22d89ac8d0c8bffe190cf2c8e11
HectorSotoG <hsoto_18@alu.uabcs.mx>
2024-06-05 17:44:35 -0700

    primerDiseñoAddGrupos

--- src/designer/AddGroups.java
+++ src/designer/AddGroups.java
@@ -1,164 +1,61 @@
-package views;
+package designer;
 
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+
+import controllers.StudentsController;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import controllers.GroupsController;
-import controllers.HomeController;
-import controllers.StudentsController;
-import designer.AddGroups;
-import designer.TeacherAdd;
-import designer.TeacherMenu;
-import models.GroupsModel;
-import models.StudentsModel;
+public class AddGroups extends JFrame {
 
-public class GroupsView {
-	
-	private JFrame frame;
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private GroupsController controller;
-	private HomeController home;
-	private GroupsModel functions;
-	private String pictureRoot = "/Icons/icons8-circled-user-100.png";
-	
-	public GroupsView() {
-		frame = new JFrame();
-		frame.setSize(700, 650);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
+	private JTextField idStudent;
+	private JTextField idClass;
+	private JTable table;
+	private JTextField idTeacher;
 
-		frame.setResizable(false);
-		frame.setVisible(false);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddGroups frame = new AddGroups();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
+	/**
+	 * Create the frame.
+	 */
+	public AddGroups() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(700,650);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		frame.setContentPane(contentPane);
-		contentPane.setLayout(new CardLayout(0, 0));
-
-		functions = new GroupsModel();
-	}
-	
-	public void menu() {
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
-		contentPane.add(panel);
-		panel.setLayout(null);
-
-		JLabel menuTxt = new JLabel("Información Alumnos");
-		menuTxt.setHorizontalAlignment(SwingConstants.CENTER);
-		menuTxt.setFont(new Font("Lato", Font.PLAIN, 43));
-		menuTxt.setBounds(87, 15, 495, 53);
-		panel.add(menuTxt);
-
-		JButton addBtn = new JButton("");
-		addBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-
-				controller = new GroupsController();
-				controller.add();
-			}
-		});
-		addBtn.setIcon(new ImageIcon(TeacherMenu.class.getResource("/Icons/icons8-add-100.png")));
-		addBtn.setBackground(new Color(252, 209, 156));
-		addBtn.setBounds(44, 126, 166, 121);
-		panel.add(addBtn);
-
-		JButton editBtn = new JButton("");
-		editBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-
-				controller = new GroupsController();
-				controller.edit();
-			}
-		});
-		editBtn.setIcon(new ImageIcon(TeacherMenu.class.getResource("/Icons/icons8-edit-100.png")));
-		editBtn.setBackground(new Color(252, 209, 156));
-		editBtn.setBounds(254, 126, 166, 121);
-		panel.add(editBtn);
-
-		JButton searchBtn = new JButton("");
-		searchBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-
-				controller = new GroupsController();
-				controller.search();
-
-			}
-		});
-		searchBtn.setIcon(new ImageIcon(TeacherMenu.class.getResource("/Icons/icons8-search-100.png")));
-		searchBtn.setBackground(new Color(252, 209, 156));
-		searchBtn.setBounds(464, 126, 166, 121);
-		panel.add(searchBtn);
-
-		JButton backBtn = new JButton("");
-		backBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				frame.dispose();
-				home = new HomeController();
-				home.dashboard();
-
-			}
-		});
-		backBtn.setIcon(new ImageIcon(TeacherMenu.class.getResource("/Icons/icons8-back-100.png")));
-		backBtn.setBackground(new Color(252, 209, 156));
-		backBtn.setBounds(464, 351, 166, 121);
-		panel.add(backBtn);
-
-		JButton deleteBtn = new JButton("");
-		deleteBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-				controller = new GroupsController();
-				controller.delete();
-			}
-		});
-		deleteBtn.setIcon(new ImageIcon(TeacherMenu.class.getResource("/Icons/icons8-delete-100.png")));
-		deleteBtn.setBackground(new Color(252, 209, 156));
-		deleteBtn.setBounds(254, 351, 166, 121);
-		panel.add(deleteBtn);
-
-		JButton downloadBtn = new JButton("");
-		downloadBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-
-				controller = new GroupsController();
-				controller.download();
-			}
-		});
-		downloadBtn.setIcon(new ImageIcon(TeacherMenu.class.getResource("/Icons/icons8-download-100.png")));
-		downloadBtn.setBackground(new Color(252, 209, 156));
-		downloadBtn.setBounds(44, 351, 166, 121);
-		panel.add(downloadBtn);
-
-		contentPane.add(panel);
-		frame.setVisible(true);
-		frame.repaint();
-		frame.revalidate();
-
-	}
-	
-	public void add() {
+		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel navPanel = new JPanel();
@@ -231,7 +128,7 @@ public class GroupsView {
 		idStudentTxt.setBounds(9, 113, 188, 20);
 		panel.add(idStudentTxt);
 		
-		JTextField idStudent = new JTextField();
+		idStudent = new JTextField();
 		idStudent.setBackground(new Color(230, 230, 230));
 		idStudent.setBackground(new Color(230, 230, 230));
 		idStudent.setFont(new Font("Lato", Font.PLAIN, 14));
@@ -259,7 +156,7 @@ public class GroupsView {
 		addClassID.setBounds(360, 151, 89, 20);
 		panel.add(addClassID);
 		
-		JTextField idClass = new JTextField();
+		idClass = new JTextField();
 		idClass.setFont(new Font("Lato", Font.PLAIN, 14));
 		idClass.setColumns(10);
 		idClass.setBackground(new Color(230, 230, 230));
@@ -271,7 +168,7 @@ public class GroupsView {
 		idClassTxt.setBounds(9, 151, 188, 20);
 		panel.add(idClassTxt);
 		
-		JTable table = new JTable();
+		table = new JTable();
 		table.setBorder(new LineBorder(new Color(230, 230, 230)));
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
@@ -307,7 +204,7 @@ public class GroupsView {
 		addTeacherID.setBounds(364, 523, 89, 20);
 		panel.add(addTeacherID);
 		
-		JTextField idTeacher = new JTextField();
+		idTeacher = new JTextField();
 		idTeacher.setFont(new Font("Lato", Font.PLAIN, 14));
 		idTeacher.setColumns(10);
 		idTeacher.setBackground(new Color(230, 230, 230));
@@ -338,26 +235,5 @@ public class GroupsView {
 		addGroup.setBackground(new Color(252, 209, 156));
 		addGroup.setBounds(303, 566, 150, 34);
 		panel.add(addGroup);
-		
-		frame.setVisible(true);
-		frame.repaint();
-		frame.revalidate();
 	}
-	
-	public void edit() {
-		
-	}
-	
-	public void search() {
-		
-	}
-	
-	public void download() {
-		
-	}
-	
-	public void delete() {
-		
-	}
-
 }
